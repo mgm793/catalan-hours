1e9a897187c23618671c1084f4deeef71626ead6
Marc Garcia i Mullon <mgm793@gmail.com>
2020-12-10 18:08:10 +0100

    Fixing test

--- src/toCatalan/toCatalan.test.js
+++ src/toCatalan/toCatalan.test.js
@@ -2,47 +2,47 @@ import {toCatalan} from './toCatalan';
 
 describe('toCatalan', () => {
   test('en punt', () => {
-    const timeInCatalan = toCatalan({hours: 10, minutes: 0});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 10:00:00'));
     expect(timeInCatalan).toEqual('les deu en punt');
   });
   test('en punt', () => {
-    const timeInCatalan = toCatalan({hours: 13, minutes: 0});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 13:00:00'));
     expect(timeInCatalan).toEqual('la una en punt');
   });
   test('un quart', () => {
-    const timeInCatalan = toCatalan({hours: 12, minutes: 15});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 12:15:00'));
     expect(timeInCatalan).toEqual('un quart d\'una');
   });
   test('dos quarts', () => {
-    const timeInCatalan = toCatalan({hours: 10, minutes: 30});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 10:30:00'));
     expect(timeInCatalan).toEqual('dos quarts d\'onze');
   });
   test('tres quarts', () => {
-    const timeInCatalan = toCatalan({hours: 11, minutes: 45});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 11:45:00'));
     expect(timeInCatalan).toEqual('tres quarts de dotze');
   });
   test('un quart i', () => {
-    const timeInCatalan = toCatalan({hours: 11, minutes: 16});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 11:16:00'));
     expect(timeInCatalan).toEqual('passa un minut d\'un quart de dotze');
   });
   test('dos quarts i', () => {
-    const timeInCatalan = toCatalan({hours: 11, minutes: 37});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 11:37:00'));
     expect(timeInCatalan).toEqual('passen set minuts de dos quarts de dotze');
   });
   test('tres quarts i', () => {
-    const timeInCatalan = toCatalan({hours: 11, minutes: 50});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 11:50:00'));
     expect(timeInCatalan).toEqual('passen cinc minuts de tres quarts de dotze');
   });
   test('un quart menys', () => {
-    const timeInCatalan = toCatalan({hours: 11, minutes: 14});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 11:14:00'));
     expect(timeInCatalan).toEqual('falta un minut per un quart de dotze');
   });
   test('dos quarts menys', () => {
-    const timeInCatalan = toCatalan({hours: 12, minutes: 28});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 12:28:00'));
     expect(timeInCatalan).toEqual('falten dos minuts per dos quarts d\'una');
   });
   test('tres quarts menys', () => {
-    const timeInCatalan = toCatalan({hours: 12, minutes: 40});
+    const timeInCatalan = toCatalan(new Date('December 17, 1995 12:40:00'));
     expect(timeInCatalan).toEqual('falten cinc minuts per tres quarts d\'una');
   });
 });
